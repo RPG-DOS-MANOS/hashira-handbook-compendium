36973b274fc25532eee4509e8899a0ec3bd41dfb
Ryan Rodrigues <ryanpimentelbr@gmail.com>
2024-04-29 10:40:05 -0300

    Update armasUnicas.js

--- scripts/armasUnicas.js
+++ scripts/armasUnicas.js
@@ -4,7 +4,13 @@ Hooks.once("init", () => {
     CONFIG.DND5E.weaponProficienciesMap.unicoR = "unico";
     CONFIG.DND5E.weaponProficienciesMap.unicoM = "unico";
 
+    CONFIG.DND5E.weponTypes.pesado = "Armas Pesadas";
+    CONFIG.DND5E.weaponProficiencies.pesado = "Armas Pesadas";
+    CONFIG.DND5E.weaponProficienciesMap.pesadoR = "pesado";
+    CONFIG.DND5E.weaponProficienciesMap.pesadoM = "pesado";
+
     CONFIG.DND5E.weaponIds.katana = "Compendium.hashira-handbook-compendium.itens.Item.RTUJzlbLcKZIkaeD";
+    CONFIG.DND5E.weaponIds.alabarda = "Compendium.hashira-handbook-compendium.itens.Item.lYXZYxvWgahhBlpW";
     
     // etc etc
   });
